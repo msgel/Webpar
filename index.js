ee705cf0d122515229db4301f147b600f56eea91
msgel <kitkat0jali@gmail.com>
2024-11-29 18:01:40 +0800

    Files

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+
+button1.addEventListener("click", function() {
+window.location.href = "file:///C:/Users/ASUS/OneDrive/%EB%AC%B8%EC%84%9C/Attachments/Desktop/web/cutterpillow.html";
+
+});
+
+button2.addEventListener("click", function() {
+window.location.href = "file:///C:/Users/ASUS/OneDrive/%EB%AC%B8%EC%84%9C/Attachments/Desktop/web/circus.html";
+});
+
+button3.addEventListener("click", function() {
+window.location.href = "file:///C:/Users/ASUS/OneDrive/%EB%AC%B8%EC%84%9C/Attachments/Desktop/web/ultra.html";
+});
+button4.addEventListener("click", function() {
+window.location.href = "file:///C:/Users/ASUS/OneDrive/%EB%AC%B8%EC%84%9C/Attachments/Desktop/web/webpage.html";
+});
+});
